29ab411d8690c91de68cef60c7f3f958a280ddef
Roilan Lambert <roilan.lambert5@gmail.com>
2025-11-24 23:42:57 -0500

    added token verification to de organization creation route

--- app/models/organization.py
+++ app/models/organization.py
@@ -5,3 +5,20 @@ class CreateOrganization(BaseModel):
     name: str
     address: str
     website: str | None = None
+
+class UpdateOrganization(BaseModel):
+    name: str | None = None
+    address: str | None = None
+    website: str | None = None
+
+
+class OrganizationResponse(BaseModel):
+    id: str
+    manager_id: str
+    name: str
+    address: str
+    website: str | None = None
+    created_at: str
+    updated_at: str
+    is_active: bool
+
